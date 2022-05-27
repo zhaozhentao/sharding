447864aba884818a64b43659ce45daed02e993b7
zhaozhentao <qq344696734@gmail.com>
2022-05-27 21:23:29 +0800

    🎨 改成yml模式

--- src/main/java/hi/Application.java
+++ src/main/java/hi/Application.java
@@ -4,9 +4,7 @@ import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-    DruidDataSourceAutoConfigure.class,
-})
+@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class})
 public class Application {
 
     public static void main(String[] args) {
