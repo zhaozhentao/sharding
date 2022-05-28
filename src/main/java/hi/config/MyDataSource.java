59e2e935303d2c5ac7de44a519f7dd99a292d64b
zhaozhentao <qq344696734@gmail.com>
2022-05-28 08:40:46 +0800

    🎨 修改连接数

--- src/main/java/hi/config/MyDataSource.java
+++ src/main/java/hi/config/MyDataSource.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 public class MyDataSource extends DruidDataSource {
 
     public MyDataSource() throws SQLException {
+        setMaxActive(Runtime.getRuntime().availableProcessors() * 8);
         setFilters("stat, wall");
     }
 }
