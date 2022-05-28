eb22f429356dc7f3f652055ca39c0bb793263e12
zhaozhentao <qq344696734@gmail.com>
2022-05-28 21:58:16 +0800

    🎨 注入间隔

--- src/main/java/hi/sharding/algorithm/DateShardingAlgorithm.java
+++ src/main/java/hi/sharding/algorithm/DateShardingAlgorithm.java
@@ -4,15 +4,25 @@ import org.apache.shardingsphere.api.sharding.standard.PreciseShardingAlgorithm;
 import org.apache.shardingsphere.api.sharding.standard.PreciseShardingValue;
 import org.apache.shardingsphere.api.sharding.standard.RangeShardingAlgorithm;
 import org.apache.shardingsphere.api.sharding.standard.RangeShardingValue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.Collections;
 
+@Component
 public class DateShardingAlgorithm implements PreciseShardingAlgorithm<Long>, RangeShardingAlgorithm<Long> {
+
+    public static Integer interval;
+
+    @Value("${spring.shardingsphere.sharding.tables.user.interval}")
+    public void setInterval(Integer interval) {
+        DateShardingAlgorithm.interval = interval;
+    }
 
     @Override
     public String doSharding(Collection<String> availableTargetNames, PreciseShardingValue<Long> shardingValue) {
-        System.out.println("这里啦");
+        System.out.println("这里啦 " + interval);
         System.out.println(availableTargetNames);
         System.out.println("shardingValue");
         System.out.println(shardingValue);
