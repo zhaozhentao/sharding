3718a9c7a0ce8f211bac1b6f01eaff76c64ef65d
zhaozhentao <qq344696734@gmail.com>
2022-05-29 15:56:51 +0800

    去掉多余的代码

--- src/main/java/hi/Application.java
+++ src/main/java/hi/Application.java
@@ -6,12 +6,10 @@ import com.alibaba.druid.spring.boot.autoconfigure.stat.DruidFilterConfiguration
 import com.alibaba.druid.spring.boot.autoconfigure.stat.DruidStatViewServletConfiguration;
 import hi.mapper.UserMapper;
 import hi.sharding.algorithm.DateShardingAlgorithm;
-import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Import;
 
 @EnableConfigurationProperties({DruidStatProperties.class})
@@ -20,17 +18,13 @@ import org.springframework.context.annotation.Import;
     DruidFilterConfiguration.class
 })
 @SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class})
-public class Application implements ApplicationContextAware {
+public class Application {
 
     public static ApplicationContext context;
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+        context = SpringApplication.run(Application.class, args);
 
-    @Override
-    public void setApplicationContext(ApplicationContext context) throws BeansException {
-        Application.context = context;
         String key = "spring.shardingsphere.sharding.tables.user.interval";
         DateShardingAlgorithm.interval = context.getEnvironment().getProperty(key, Integer.class, 5000);
         DateShardingAlgorithm.tables = context.getBean(UserMapper.class).loadTables();
